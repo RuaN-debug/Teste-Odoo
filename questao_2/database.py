a6cba7e15a32d509c76fc0c00b9e64d3179bfa7e
Ruan Marcos <ruanmbalbino@gmail.com>
2023-09-25 22:26:32 -0300

    database configuration

---
+++ questao_2/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+import os
+
+# Environment variables
+load_dotenv('./.env')
+user = os.environ.get("POSTGRES_USER")
+password = os.environ.get("POSTGRES_PASSWORD")
+database_name = os.environ.get("POSTGRES_DB")
+port = os.environ.get("POSTGRES_PORT")
+
+# Database configuration
+DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{port}/{database_name}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
