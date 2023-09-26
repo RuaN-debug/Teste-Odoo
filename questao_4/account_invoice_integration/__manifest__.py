67be96c66bee9d7a255204516f4070625aba7db7
Ruan Marcos <ruanmbalbino@gmail.com>
2023-09-26 20:23:46 -0300

    creating new module

---
+++ questao_4/account_invoice_integration/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Account Invoice Integration",
+    "summary": "Integração de Faturas com API externa",
+    "Author": "Ruan Marcos",
+    "version": "1.0.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_invoice_integration_view.xml",
+    ],
+    "installable": "True",
+}
